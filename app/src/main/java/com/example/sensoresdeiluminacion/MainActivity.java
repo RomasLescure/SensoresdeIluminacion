b6fb036d91d250ea1642f6364f36d253017a3cf5
juant14 <105187326+juant14@users.noreply.github.com>
2023-07-13 21:50:47 -0500

    Sensor de iluminación y flash
    solo falta ponerlo bonito

--- app/src/main/java/com/example/sensoresdeiluminacion/MainActivity.java
+++ app/src/main/java/com/example/sensoresdeiluminacion/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.sensoresdeiluminacion;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private TextView lightLevelTextView;
+    private CameraManager cameraManager;
+    private String cameraId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lightLevelTextView = findViewById(R.id.lightLevelTextView);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            cameraId = cameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            float lightLevel = event.values[0];
+            lightLevelTextView.setText("Nivel de Iluminación: " + lightLevel);
+
+            if (lightLevel < 8) {
+                // Bajos niveles de iluminación
+                try {
+                    cameraManager.setTorchMode(cameraId, true); // Enciende el flash
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                // Altos niveles de iluminación
+                try {
+                    cameraManager.setTorchMode(cameraId, false); // Apaga el flash
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
