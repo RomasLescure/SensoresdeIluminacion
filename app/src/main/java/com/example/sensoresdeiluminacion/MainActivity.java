728fc93bab9f48d7c32e421f9662d08e4fa7c0d3
juant14 <105187326+juant14@users.noreply.github.com>
2023-07-23 19:38:23 -0500

    Commit, jueguito

--- app/src/main/java/com/example/sensoresdeiluminacion/MainActivity.java
+++ app/src/main/java/com/example/sensoresdeiluminacion/MainActivity.java
@@ -9,14 +9,24 @@ import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager sensorManager;
     private Sensor lightSensor;
     private TextView lightLevelTextView;
+    private TextView instructionTextView;
+    private Button playAgainButton;
     private CameraManager cameraManager;
     private String cameraId;
+
+    private boolean shouldWin; // Variable para saber si debe ganar o no
+    private boolean isGameRunning = true; // Variable para saber si el juego está en ejecución
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         lightLevelTextView = findViewById(R.id.lightLevelTextView);
+        instructionTextView = findViewById(R.id.instructionTextView);
+        playAgainButton = findViewById(R.id.playAgainButton);
+        playAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isGameRunning) {
+                    playAgain();
+                }
+            }
+        });
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
@@ -35,6 +55,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             e.printStackTrace();
         }
 
+        // Generar un valor aleatorio inicial para shouldWin (true: ganar, false: perder)
+        updateShouldWin();
+
         sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
@@ -44,15 +67,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             float lightLevel = event.values[0];
             lightLevelTextView.setText("Nivel de Iluminación: " + lightLevel);
 
-            if (lightLevel < 15) {
-                // Bajos niveles de iluminación
+            if (isGameRunning && lightLevel < 16 && shouldWin) {
+                // Bajos niveles de iluminación y se debe ganar
                 try {
                     cameraManager.setTorchMode(cameraId, true); // Enciende el flash
+                    showWinToast();
+                    isGameRunning = false; // Detener el juego después de ganar
+                    instructionTextView.setText(""); // Eliminar el mensaje de "oscuro" o "claro"
                 } catch (CameraAccessException e) {
                     e.printStackTrace();
                 }
-            } else {
-                // Altos niveles de iluminación
+            } else if (isGameRunning && lightLevel >= 16 && !shouldWin) {
+                // Altos niveles de iluminación y se debe ganar
+                try {
+                    cameraManager.setTorchMode(cameraId, true); // Enciende el flash
+                    showWinToast();
+                    isGameRunning = false; // Detener el juego después de ganar
+                    instructionTextView.setText(""); // Eliminar el mensaje de "oscuro" o "claro"
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            } else if (!isGameRunning) {
+                // Juego detenido, apagar el flash
                 try {
                     cameraManager.setTorchMode(cameraId, false); // Apaga el flash
                 } catch (CameraAccessException e) {
@@ -65,5 +101,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    private void showWinToast() {
+        Toast.makeText(this, "¡Ganaste!", Toast.LENGTH_LONG).show();
+    }
+
+    private void updateShouldWin() {
+        // Generar un nuevo valor aleatorio para shouldWin
+        Random random = new Random();
+        shouldWin = random.nextBoolean();
+    }
+
+    private void playAgain() {
+        // Volver a generar un valor aleatorio y actualizar el mensaje
+        updateShouldWin();
+        instructionTextView.setText(shouldWin ? "claro" : "oscuro"); // Mostrar el mensaje "oscuro" o "claro" nuevamente
+        isGameRunning = true; // Volver a habilitar el juego
     }
 }
